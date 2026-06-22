9812e5e74671ad1581389cbd3f3a7aa5bff3b186
Olav van Rabenswaaij <olav.van.rabenswaaij@gmail.com>
2026-06-22 09:23:39 +0200

    update main.py and base templates

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ logger.addHandler(_log_handler)
 
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 client = commands.Bot(command_prefix='!', intents=intents)
 
 
